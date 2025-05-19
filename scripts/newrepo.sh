1c67c72037209155e27f8ed123882396b7d47734
tomlloydDEV <tom111b@hotmail.co.uk>
2025-05-19 15:42:18 +0100

    add repo script change name of alacritty script

---
+++ scripts/newrepo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script creates a new repository on GitHub and initializes it with a README file.
+newrepo() {
+    mkdir "$1" && cd "$1" || exit 1
+    git init
+    echo "# $1" >> README.md
+    git add .
+    git commit -m "Init"
+    gh repo create "$1" --private --source=. --push
+}
