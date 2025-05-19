c22a7cc55ea4ba93497fe6b7e7be30f4f7197861
tomlloydDEV <tom111b@hotmail.co.uk>
2025-05-19 21:22:02 +0100

    add functions to bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,14 +2,17 @@
 
 set -eu
 
-echo "Bootstrapping your development enviroment..."
-
-# core packages
 install_packages() {
-  local packages=(git curl nvim tmux python3 python3-pip)
+  local packages=(git curl wget python3 python3-pip nvim tmux btop tldr fzf ripgrep jq yq)
   echo "Installing core packages..."
   sudo apt update && sudo apt install -y "${packages[@]}"
   echo "Core packages installed."
 }
 
+link_dotfiles() {...}
 
+setup_git() {...}
+
+configure_vscode() {...}
+
+secure_machine() {...}
